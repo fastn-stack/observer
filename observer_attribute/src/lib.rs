6aff97995287f93f6ce64d2a279720cdb0fe6701
AbrarNitk <abrar.nitk@gmail.com>
2019-07-25 18:09:55 +0530

    user handling of a response

--- observer_attribute/src/lib.rs
+++ observer_attribute/src/lib.rs
@@ -21,6 +21,7 @@ use syn::Item;
 #[derive(Debug, Deserialize, Clone)]
 struct Event {
     critical: bool,
+    result_type: String,
     fields: HashMap<String, String>,
 }
 
@@ -36,7 +37,9 @@ lazy_static! {
 }
 
 #[proc_macro_attribute]
-pub fn observed(_metadata: TokenStream, input: TokenStream) -> TokenStream {
+pub fn observed(metadata: TokenStream, input: TokenStream) -> TokenStream {
+    let parse_meta: syn::Meta = syn::parse(metadata).expect("Failed to parse metadata");
+    let meta = parse_meta.name().to_string();
     let item: syn::Item = syn::parse(input).expect("failed to parse input");
     let function = get_fn(item);
 
@@ -48,14 +51,25 @@ pub fn observed(_metadata: TokenStream, input: TokenStream) -> TokenStream {
     let table_name = ident.to_string();
     let block = rewrite_func_block(block, &table_name);
     let is_critical = get_event(&table_name).critical;
-    (quote! {
+    if meta.as_str() == "with_result" {
+        (quote! {
         #visibility fn #ident(#inputs) #output {
-            observe(ctx, #table_name, #is_critical, || {
+            observe_with_result(ctx, #table_name, #is_critical, || {
                 #block
             })
         }
-    })
-    .into()
+        })
+        .into()
+    } else {
+        (quote! {
+        #visibility fn #ident(#inputs) #output {
+            observe_all(ctx, #table_name, #is_critical, || {
+                #block
+            })
+        }
+        })
+        .into()
+    }
 }
 
 fn rewrite_func_block(mut block: Box<syn::Block>, table_name: &str) -> Box<syn::Block> {
@@ -66,11 +80,12 @@ fn rewrite_func_block(mut block: Box<syn::Block>, table_name: &str) -> Box<syn::
             syn::Stmt::Semi(e, s) => match e {
                 //                syn::Expr::Macro(m) => {
                 //                    let mut new_macro = m.clone();
-                //                    if m.mac.path.segments[0].ident.to_string().eq("println") {
+                //                    if m.mac.path.segments[0].ident.to_string().eq("observe_result") {
                 //                        new_macro.mac.path.segments[0].ident =
-                //                            syn::Ident::new("format", Span::call_site());
+                //                            syn::Ident::new("observe_result_i32", Span::call_site());
                 //                    }
-                //                    stmts.push(syn::Stmt::Semi(syn::Expr::Macro(new_macro), s));
+                //                    // stmts.push(syn::Stmt::Semi(syn::Expr::Macro(new_macro), s));
+                //
                 //                }
                 syn::Expr::Call(c) => {
                     let call = c.clone();
@@ -78,7 +93,6 @@ fn rewrite_func_block(mut block: Box<syn::Block>, table_name: &str) -> Box<syn::
                     match *c.func {
                         syn::Expr::Path(p) => {
                             let mut path = p.clone();
-                            // if p.path.segments[0].ident.to_string().eq("observed_field!") { TODO
                             if p.path.segments[0].ident.to_string().eq("observe_field") {
                                 if let syn::Expr::Lit(l) = args[1].clone() {
                                     if let syn::Lit::Str(s) = l.lit.clone() {
@@ -89,6 +103,14 @@ fn rewrite_func_block(mut block: Box<syn::Block>, table_name: &str) -> Box<syn::
                                     }
                                 }
                             }
+
+                            if p.path.segments[0].ident.to_string().eq("observe_result") {
+                                let f_name =
+                                    "observe_result_".to_string() + &get_result_type(table_name);
+                                path.path.segments[0].ident =
+                                    syn::Ident::new(&f_name, Span::call_site());
+                            }
+
                             stmts.push(syn::Stmt::Semi(
                                 syn::Expr::Call(syn::ExprCall {
                                     attrs: call.attrs,
@@ -152,13 +174,13 @@ fn get_struct_name(item: syn::Item) -> String {
 fn get_event(table: &str) -> Event {
     match EVENTS.get(table) {
         Some(e) => e.clone(),
-        None => panic!("No table named \"{}\" in the events.json file", table),
+        None => panic!("No table named \"{}\" in the events file", table),
     }
 }
 
 fn get_func(field: String, table: &str) -> String {
     match get_event(table).fields.get(&field) {
-        Some(t) => get_rust_type(t.to_string()),
+        Some(t) => get_rust_type(t),
         None => panic!(
             "No field named \"{}\" in the fields for the table \"{}\"",
             field, table
@@ -166,14 +188,12 @@ fn get_func(field: String, table: &str) -> String {
     }
 }
 
-fn get_rust_type(storage_type: String) -> String {
-    if storage_type.to_lowercase().eq("int") {
-        return "i32".to_string();
-    } else if storage_type.to_lowercase().eq("string") {
-        return "string".to_string();
-    } else {
-        return "string".to_string();
-    }
+fn get_result_type(table: &str) -> String {
+    get_event(table).result_type.to_lowercase()
+}
+
+fn get_rust_type(storage_type: &str) -> String {
+    storage_type.to_lowercase()
 }
 
 //fn get_table_name(metadata: String) -> String {
