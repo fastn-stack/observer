a0a642fed9076d9f7c5fc8d2b3214464a3fdb524
AbrarNitk <abrar.nitk@gmail.com>
2019-08-01 15:34:57 +0530

    remove extra comments

--- src/observe.rs
+++ src/observe.rs
@@ -10,34 +10,24 @@ where
     F: FnOnce() -> Result<T>,
 {
     start_frame(table_name);
-    //ctx.start_frame(table_name);
     match run() {
         Ok(r) => {
             end_frame(is_critical, None);
-            // ctx.end_frame(is_critical, None);
             Ok(r)
         }
         Err(e) => {
             end_frame(is_critical, Some(e.to_string()));
-            //            ctx.end_frame(is_critical, Some(e.to_string()));
             Err(e)
         }
     }
 }
 
-pub fn observe_all<F, T>(
-    // ctx: &Context,
-    table_name: &str,
-    is_critical: bool,
-    run: F,
-) -> T
+pub fn observe_all<F, T>(table_name: &str, is_critical: bool, run: F) -> T
 where
     F: FnOnce() -> T,
 {
     start_frame(table_name);
-    // ctx.start_frame(table_name);
     let result = run();
     end_frame(is_critical, None);
-    // ctx.end_frame(is_critical, None);
     result
 }
