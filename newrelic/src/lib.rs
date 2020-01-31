034835dcc33495b501222fb51bb709281f7a3a2e
Abrar Khan <abrar.nitk@gmail.com>
2020-01-31 14:38:36 +0530

    made observer object as lazy-static and made context as thread_local (#7)

--- newrelic/src/lib.rs
+++ newrelic/src/lib.rs
@@ -1,49 +1,49 @@
 pub struct ObserverNewRelic {
-    _segment_stack: std::cell::RefCell<Vec<ackorelic::acko_segment::Segment>>,
+    _segment_stack: std::sync::Arc<Vec<ackorelic::acko_segment::Segment>>,
 }
 
 impl ObserverNewRelic {
     pub fn new() -> Self {
         ObserverNewRelic {
-            _segment_stack: std::cell::RefCell::new(vec![]),
+            _segment_stack: std::sync::Arc::new(vec![]),
         }
     }
 }
 
-/// Implementation of Backend trait for NewRelic
-impl observer::Backend for ObserverNewRelic {
-    /// This will start NewRelic app
-    fn app_started(&self) {}
-    /// This will end NewRelic app
-    fn app_ended(&self) {}
-    /// This method will be called when context has been created.
-    fn context_created(&self, id: &str) {
-        // Need to create web transaction of NewRelic
-        // ackorelic::newrelic_fn::nr_start_web_transaction(id);
-        println!("ObserverNewRelic: Context Started: {}", id);
-    }
-    /// This method will be called when context ended.
-    fn context_ended(&self, _ctx: &observer::Context) {
-        // Need to end web transaction
-        // ackorelic::newrelic_fn::nr_end_transaction()
-        println!("ObserverNewRelic: Context Ended")
-    }
-    /// This method will be when span created.
-    fn span_created(&self, id: &str) {
-        // Need to start a newrelic segment and store it stack
-        //        self.segment_stack
-        //            .borrow_mut()
-        //            .push(ackorelic::newrelic_fn::nr_start_custom_segment(id))
-        println!("ObserverNewRelic: Span Created: {}", id)
-    }
-    /// This method will be when span needs to logged.
-    fn span_data(&self, _key: &str, _value: &str) {}
-    /// This method will be when span ended.
-    fn span_ended(&self, _span: Option<&observer::span::Span>) {
-        // Needs to end a segment which was stored earlier in stack
-        //        if let Some(segment) = self.segment_stack.borrow_mut().pop() {
-        //            ackorelic::newrelic_fn::nr_end_custom_segment(segment);
-        //        }
-        println!("ObserverNewRelic: Span Ended")
-    }
-}
+///// Implementation of Backend trait for NewRelic
+//impl observer::Backend for ObserverNewRelic {
+//    /// This will start NewRelic app
+//    fn app_started(&self) {}
+//    /// This will end NewRelic app
+//    fn app_ended(&self) {}
+//    /// This method will be called when context has been created.
+//    fn context_created(&self, id: &str) {
+//        // Need to create web transaction of NewRelic
+//        // ackorelic::newrelic_fn::nr_start_web_transaction(id);
+//        println!("ObserverNewRelic: Context Started: {}", id);
+//    }
+//    /// This method will be called when context ended.
+//    fn context_ended(&self, _ctx: &observer::Context) {
+//        // Need to end web transaction
+//        // ackorelic::newrelic_fn::nr_end_transaction()
+//        println!("ObserverNewRelic: Context Ended")
+//    }
+//    /// This method will be when span created.
+//    fn span_created(&self, id: &str) {
+//        // Need to start a newrelic segment and store it stack
+//        //        self.segment_stack
+//        //            .borrow_mut()
+//        //            .push(ackorelic::newrelic_fn::nr_start_custom_segment(id))
+//        println!("ObserverNewRelic: Span Created: {}", id)
+//    }
+//    /// This method will be when span needs to logged.
+//    fn span_data(&self, _key: &str, _value: &str) {}
+//    /// This method will be when span ended.
+//    fn span_ended(&self, _span: Option<&observer::span::Span>) {
+//        // Needs to end a segment which was stored earlier in stack
+//        //        if let Some(segment) = self.segment_stack.borrow_mut().pop() {
+//        //            ackorelic::newrelic_fn::nr_end_custom_segment(segment);
+//        //        }
+//        println!("ObserverNewRelic: Span Ended")
+//    }
+//}
--- observer/src/lib.rs
+++ observer/src/lib.rs
@@ -21,15 +21,18 @@ pub mod pg;
 pub mod prelude;
 pub mod span;
 mod sql_parse;
+
 pub use crate::context::Context;
+
 #[macro_use]
 extern crate log;
 
 #[cfg(test)]
 mod tests;
+
 pub type Result<T> = std::result::Result<T, failure::Error>;
 
-pub trait Backend {
+pub trait Backend: Send + Sync {
     fn app_started(&self);
     fn app_ended(&self);
     fn context_created(&self, id: &str);
@@ -41,105 +44,96 @@ pub trait Backend {
 
 pub struct Observer {
     backends: Vec<Box<dyn Backend>>,
-    context: std::cell::RefCell<Box<Option<crate::Context>>>,
+}
+
+lazy_static! {
+    static ref OBSERVER: std::sync::Arc<std::sync::RwLock<Option<Observer>>> = std::sync::Arc::new(std::sync::RwLock::new(None));
 }
 
 thread_local! {
-    static OBSERVER: std::cell::RefCell<Option<Observer>> = std::cell::RefCell::new(None);
+    static CONTEXT: std::cell::RefCell<Option<Context>> = std::cell::RefCell::new(None);
 }
 
 pub fn builder(backend: Box<dyn Backend>) -> Observer {
     Observer::builder(backend)
 }
 
-//pub fn create_observer(backends: Vec<Box<dyn Backend>>) {
-//    OBSERVER.with(|observer| {
-//        let mut observer = observer.borrow_mut();
-//        observer.replace(Observer::new(backends))
-//    });
-//}
-
 pub fn create_context(context_id: &str) {
-    println!("creating context 0");
-    OBSERVER.with(|observer| {
-        if let Some(obj) = observer.borrow().as_ref() {
-            println!("creating context");
-            obj.create_context(context_id);
+    match OBSERVER.as_ref().read() {
+        Ok(obj) => {
+            if let Some(obj) = obj.as_ref() {
+                obj.create_context(context_id);
+            }
         }
-    });
+        Err(_err) => {}
+    };
 }
 
 pub fn end_context() {
-    OBSERVER.with(|observer| {
-        if let Some(obj) = observer.borrow().as_ref() {
-            obj.end_context();
+    match OBSERVER.as_ref().read() {
+        Ok(obj) => {
+            if let Some(obj) = obj.as_ref() {
+                obj.end_context();
+            }
         }
-        let mut observer = observer.borrow_mut();
-        observer.take();
-    });
+        Err(_err) => {}
+    };
 }
 
 pub fn observe_span_log(value: &str) {
-    OBSERVER.with(|observer| {
-        if let Some(obj) = observer.borrow().as_ref() {
-            obj.span_log(value);
+    match OBSERVER.as_ref().read() {
+        Ok(obj) => {
+            if let Some(obj) = obj.as_ref() {
+                obj.span_log(value);
+            }
         }
-    });
+        Err(_err) => {}
+    };
 }
 
 pub(crate) fn start_span(id: &str) {
-    OBSERVER.with(|observer| {
-        if let Some(obj) = observer.borrow().as_ref() {
-            obj.create_span(id);
+    match OBSERVER.as_ref().read() {
+        Ok(obj) => {
+            if let Some(obj) = obj.as_ref() {
+                obj.create_span(id);
+            }
         }
-    });
+        Err(_err) => {}
+    };
 }
 
 pub(crate) fn end_span(is_critical: bool, err: Option<String>) {
-    OBSERVER.with(|observer| {
-        if let Some(obj) = observer.borrow().as_ref() {
-            obj.end_span(is_critical, err);
+    match OBSERVER.as_ref().read() {
+        Ok(obj) => {
+            if let Some(obj) = obj.as_ref() {
+                obj.end_span(is_critical, err);
+            }
         }
-    });
+        Err(_err) => {}
+    };
 }
 
-//pub(crate) fn end_ctx_frame() {
-//    OBSERVER.with(|observer| {
-//        if let Some(obj) = observer.borrow().as_ref() {
-//            if let Some(ctx) = obj.context.borrow().as_ref() {
-//                ctx.end_ctx_frame();
-//            }
-//        }
-//    });
-//}
-
 pub(crate) fn observe_field(key: &str, value: serde_json::Value) {
-    OBSERVER.with(|observer| {
-        if let Some(obj) = observer.borrow().as_ref() {
-            if let Some(ctx) = obj.context.borrow().as_ref() {
-                ctx.observe_span_field(key, value)
-            }
+    CONTEXT.with(|context| {
+        if let Some(ctx) = context.borrow().as_ref() {
+            ctx.observe_span_field(key, value);
         }
     });
 }
 
 pub(crate) fn observe_result(result: impl serde::Serialize) {
-    OBSERVER.with(|observer| {
-        if let Some(obj) = observer.borrow().as_ref() {
-            if let Some(ctx) = obj.context.borrow().as_ref() {
-                ctx.observe_span_result(result);
-            }
+    CONTEXT.with(|ctx| {
+        if let Some(ctx) = ctx.borrow().as_ref() {
+            ctx.observe_span_result(result);
         }
     });
 }
 
 #[allow(dead_code)]
 pub(crate) fn observe_span_id(id: &str) {
-    OBSERVER.with(|observer| {
-        if let Some(obj) = observer.borrow().as_ref() {
-            if let Some(ctx) = obj.context.borrow().as_ref() {
-                ctx.observe_span_id(id);
-            }
+    CONTEXT.with(|context| {
+        if let Some(ctx) = context.borrow().as_ref() {
+            ctx.observe_span_id(id);
         }
     });
 }
@@ -151,7 +145,6 @@ impl Observer {
     pub fn builder(backend: Box<dyn Backend>) -> Self {
         Observer {
             backends: vec![backend],
-            context: std::cell::RefCell::new(Box::new(None)),
         }
     }
 
@@ -165,76 +158,68 @@ impl Observer {
             backend.app_started()
         }
 
-        OBSERVER.with(|observer| {
-            let mut observer = observer.borrow_mut();
-            observer.replace(self)
-        });
+        match OBSERVER.write() {
+            Ok(mut obj) => {
+                obj.replace(self);
+            }
+            Err(_e) => {}
+        };
     }
 
-    //    pub fn new(backends: Vec<Box<dyn Backend>>) -> Self {
-    //        for backend in backends.iter() {
-    //            backend.app_started()
-    //        }
-    //        Observer {
-    //            backends,
-    //            context: std::cell::RefCell::new(Box::new(None)),
-    //            log_path: None,
-    //            stdout: false,
-    //        }
-    //    }
-
     /// It will iterate through all backends and call their context_created method.
-    pub fn create_context(&self, context_id: &str) {
-        let mut context = self.context.borrow_mut();
-        if context.is_none() {
-            context.replace(crate::context::Context::new(context_id.to_string()));
+    pub(crate) fn create_context(&self, context_id: &str) {
+        CONTEXT.with(|obj| {
+            let mut context = obj.borrow_mut();
+            if context.is_none() {
+                context.replace(Context::new(context_id.to_string()));
+            }
             for backend in self.backends.iter() {
                 backend.context_created(context_id);
             }
-        }
+        });
     }
-
-    //    fn create_ctx(&self, context_id: &str) {
-    //        let mut context = self.context.borrow_mut();
-    //        if context.is_none() {
-    //            context.replace(crate::context::Context::new(context_id.to_string()));
-    //            for backend in self.backends.iter() {
-    //                backend.context_created(context_id);
-    //            }
-    //        }
-    //    }
 
     /// It will end context object and drop things if needed.
     pub(crate) fn end_context(&self) {
-        if let Some(ctx) = self.context.borrow().as_ref() {
-            ctx.finalise();
-            for backend in self.backends.iter() {
-                backend.context_ended(&ctx);
+        CONTEXT.with(|ctx| {
+            let mut ctx = ctx.borrow_mut();
+            if let Some(ctx) = ctx.as_ref() {
+                let _ = ctx.finalise();
+                for backend in self.backends.iter() {
+                    backend.context_ended(&ctx);
+                }
             }
-        }
+            ctx.take();
+        });
     }
 
     pub(crate) fn create_span(&self, id: &str) {
-        if let Some(ctx) = self.context.borrow().as_ref() {
-            ctx.start_span(id);
-        }
-        for backend in self.backends.iter() {
-            backend.span_created(id);
-        }
+        CONTEXT.with(|ctx| {
+            if let Some(ctx) = ctx.borrow().as_ref() {
+                ctx.start_span(id);
+                for backend in self.backends.iter() {
+                    backend.span_created(id);
+                }
+            }
+        });
     }
 
     pub(crate) fn end_span(&self, is_critical: bool, err: Option<String>) {
-        if let Some(ctx) = self.context.borrow().as_ref() {
-            ctx.end_span(is_critical, err);
-            for backend in self.backends.iter() {
-                backend.span_ended(ctx.span_stack.borrow().last());
+        CONTEXT.with(|ctx| {
+            if let Some(ctx) = ctx.borrow().as_ref() {
+                ctx.end_span(is_critical, err);
+                for backend in self.backends.iter() {
+                    backend.span_ended(ctx.span_stack.borrow().last());
+                }
             }
-        }
+        });
     }
 
     pub(crate) fn span_log(&self, value: &str) {
-        if let Some(ctx) = self.context.borrow().as_ref() {
-            ctx.span_log(value);
-        }
+        CONTEXT.with(|ctx| {
+            if let Some(ctx) = ctx.borrow().as_ref() {
+                ctx.span_log(value);
+            }
+        });
     }
 }
